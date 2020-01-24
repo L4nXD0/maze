634adc65911659fab57a1d66d48a753498a8774d
L4nXD0 <60175395+L4nXD0@users.noreply.github.com>
2020-01-24 22:33:58 +0100

    lampen lag, small asset changes

---
+++ Maze/Assets/LampOnOff.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampOnOff : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Maze/Assets/LampOnOffEG.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampOnOffEG : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine("TurnLampsOff");
+
+        }
+    }
+    IEnumerator TurnLampsOff()
+    {
+         foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+        return null;
+    }
+}
